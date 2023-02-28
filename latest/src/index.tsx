5fa28376de982b5c783974bbbdeb8d0d5b6e7514
OrJDev <orjdeveloper@gmail.com>
2023-02-28 08:12:34 +0200

    move to @tanstack/solid-query@alpha

---
+++ latest/src/index.tsx
@@ -0,0 +1,3 @@
+export * from "@trpc/client";
+export { createTRPCSolid, type CreateTRPCSolid } from "./createTRPCSolid";
+export { createSolidQueryHooks } from "./interop";
---
+++ latest/src/shared/types.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { type QueryClient } from "@tanstack/solid-query";
+import { type AnyRouter, type MaybePromise } from "@trpc/server";
+import type Solid from "solid-js";
+
+/**
+ * @internal
+ */
+export interface UseMutationOverride {
+  onSuccess: (opts: {
+    /**
+     * Calls the original function that was defined in the query's `onSuccess` option
+     */
+    originalFn: () => MaybePromise<unknown>;
+    queryClient: QueryClient;
+  }) => MaybePromise<unknown>;
+}
+
+/**
+ * @internal
+ */
+export interface CreateTRPCSolidOptions<_TRouter extends AnyRouter> {
+  /**
+   * Override behaviors of the built-in hooks
+   */
+  unstable_overrides?: {
+    useMutation?: Partial<UseMutationOverride>;
+  };
+
+  /**
+   * Override the default context provider
+   * @default undefined
+   */
+  context?: Solid.Context<any>;
+  /**
+   * Override the default React Query context
+   * @default undefined
+   */
+  solidQueryContext?: Solid.Context<QueryClient | undefined>;
+}
